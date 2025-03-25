e41a876ef406b292f6ba0ba9999c597a6d2d53b3
matthias42592 <matthias190e@gmail.com>
2025-03-25 23:53:07 +0100

    updated file firebase_config

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,17 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, db
+import json
 
-# Firebase Service Key laden
-cred = credentials.Certificate("firebase-key.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://foodmenu-d3081-default-rtdb.europe-west1.firebasedatabase.app"
-})
+# 🔐 Lade den Schlüssel aus einer Umgebungsvariablen
+firebase_key = os.environ.get("FIREBASE_KEY_JSON")
 
-# Verbindung zur Firebase Realtime Database
-database = db.reference()
+if firebase_key:
+    key_dict = json.loads(firebase_key)
+    cred = credentials.Certificate(key_dict)
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': "https://foodmenu-d3081-default-rtdb.europe-west1.firebasedatabase.app"
+    })
+    database = db.reference()
+else:
+    raise Exception("FIREBASE_KEY_JSON not set!")
